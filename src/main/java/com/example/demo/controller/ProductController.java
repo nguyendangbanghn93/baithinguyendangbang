b57606947ebc7b0ade53357a34c139bfee38a12e
Nguyen Dang Bang <bang@gmail.com>
2021-06-28 21:32:19 +0700

    controller

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,2 +1,23 @@
-package com.example.demo.controller;public class ProductController {
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/products")
+public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Product> list() {
+        return productRepository.findAll();
+    }
 }
